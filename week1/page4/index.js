20279783c36e9709ae5f0ea52a1bfbda7effdba4
dahye-program <dahye0729@sch.ac.kr>
2022-07-05 17:37:03 +0900

    Add : page4 퍼블리싱 (todo: icon imgs 추가, 반응형 적용)

---
+++ week1/page4/index.js
@@ -0,0 +1,18 @@
+import {CustomersData} from './Data/CustomersData.js';
+import {OrdersData} from './Data/OrdersData.js';
+
+// Recent Orders data
+const orderstable = document.querySelector('.orders_wrap');
+let orderslist = OrdersData.map((value)=>{
+  return '<ul class="orders_table_content"><li class="orders_name">'+value.Name+'</li><li class="orders_price">'+value.Price+'</li><li class="orders_pament">'+value.Pament+'</li><li class="orders_status">'+value.Status+'</li></ul>';
+})
+
+orderstable.innerHTML = orderslist.join('');
+
+// Recent Customers data
+const customers = document.querySelector('.customers_info');
+let list = CustomersData.map((value)=>{
+  return '<div class="customers_name">'+value.Name+'</div>'+'<div class="customers_country">'+value.Country+'</div>'
+})
+
+customers.innerHTML = list.join('');
