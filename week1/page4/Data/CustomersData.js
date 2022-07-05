df6ba787b53c64060b2382286129498e35cacb11
dahye-program <dahye0729@sch.ac.kr>
2022-07-05 11:14:20 +0900

    Add : list data

---
+++ week1/page4/Data/CustomersData.js
@@ -0,0 +1,34 @@
+export const CustomersData = [
+  {
+    Name: 'David',
+    Country: 'Italy'
+  },
+  {
+    Name: 'Muhammad',
+    Country: 'India'
+  },
+  {
+    Name: 'Amelia',
+    Country: 'France'
+  },
+  {
+    Name: 'Olivia',
+    Country: 'USA'
+  },
+  {
+    Name: 'Amit',
+    Country: 'Japan'
+  },
+  {
+    Name: 'Ashraf',
+    Country: 'India'
+  },
+  {
+    Name: 'Diana',
+    Country: 'Malaysia'
+  },
+  {
+    Name: 'Amit',
+    Country: 'India'
+  }
+]
