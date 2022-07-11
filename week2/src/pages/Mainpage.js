61f1b063bd3e0dc45c9bb91dcdfcda1d080be1d2
dahye-program <dahye0729@sch.ac.kr>
2022-07-11 16:08:48 +0900

    Add : Mainpage

---
+++ week2/src/pages/Mainpage.js
@@ -0,0 +1,17 @@
+import Sidebar from '../components/Sidebar';
+import Header from '../components/Header';
+import Content from '../components/Content';
+import Footer from '../components/Footer';
+
+const Mainpage = () => {
+  return(
+  <>
+    <Sidebar />
+    <Header />
+    <Content />
+    <Footer />
+  </>
+  );
+}
+
+export default Mainpage;
