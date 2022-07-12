fc63f3b3a45b5396011e6ebe15ad93f26976e82b
dahye-program <dahye0729@sch.ac.kr>
2022-07-12 14:54:28 +0900

    Add : Footer

--- week2/src/components/Footer.jsx
+++ week2/src/components/Footer.jsx
@@ -4,9 +4,18 @@ import styles from './Footer.css'
 
 const Footer = () => {
   return(
-  <>
-    <h1>Footer입니다.</h1>
-  </>
+  <div className='Container'>
+    <div className='Wrapper'>
+      <div className='Info'>
+        <div className='Title'></div>
+        <div className='Text'>대표 : 양푸터 | 사업자등록번호 : 123-45-67890</div>
+        <div className='Text'>서울특별시 강남구 개포로 640 (단비교육) </div>
+        <div className='Text'>경기도 파주시 조리읍 당재봉로 14 (오산리,물류센터)</div>
+        <div className='Text'>통신판매업 신고번호 : 2022-양푸터터-푸터푸터푸터</div>
+        <div className='Text'>© Danbi Education. ALL RIGHTS RESERVED</div>
+      </div>
+    </div>
+  </div>
 )}
 
 export default Footer;
