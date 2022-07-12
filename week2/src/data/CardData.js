ea6fa2f06298aeb5f3fd2ac960db890f92441bf9
dahye-program <dahye0729@sch.ac.kr>
2022-07-12 10:38:23 +0900

    Add : CardData

---
+++ week2/src/data/CardData.js
@@ -0,0 +1,8 @@
+const CardData = [
+  { value: '1,504', title: 'Daily Views', src: 'images/dailyviews.png'},
+  { value: '80', title: 'Sales', src: 'images/sales.png' },
+  { value: '284', title: 'Comments', src: 'images/comments.png' },
+  { value: '$7,842', title: 'Earning', src: 'images/earning.jpeg'}
+]
+
+export default CardData;
