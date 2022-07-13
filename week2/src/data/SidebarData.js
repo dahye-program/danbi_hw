ec38838937ea268feb14885cafd12183d1658d02
dahye-program <dahye0729@sch.ac.kr>
2022-07-13 14:15:07 +0900

    Add : menu data

---
+++ week2/src/data/SidebarData.js
@@ -0,0 +1,46 @@
+const SidebarData = [
+  {
+    name: 'Dashboard',
+    src: 'images/dashboard.png',
+    to: '/dashboard',
+    content: 'dashboard'
+  },
+  {
+    name: 'Customers',
+    src: 'images/customers.jpeg',
+    to: '/customers',
+    content: 'customers'
+  },
+  {
+    name: 'Message',
+    src: 'images/message.png',
+    to: '/message',
+    content: 'message'
+  },
+  {
+    name: 'Help',
+    src: 'images/help.png',
+    to: '/help',
+    content: 'help'
+  },
+  {
+    name: 'Settings',
+    src: 'images/settings.png',
+    to: '/settings',
+    content: 'settings'
+  },
+  {
+    name: 'Password',
+    src: 'images/password.png',
+    to: '/password',
+    content: 'password'
+  },
+  {
+    name: 'Sign out',
+    src: 'images/signout.jpeg',
+    to: '/signout',
+    content: 'signout'
+  }
+]
+
+export default SidebarData;
