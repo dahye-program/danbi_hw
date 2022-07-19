936d4e8ae39a210f87b5e30cab181dfbb82f8525
dahye-program <dahye0729@sch.ac.kr>
2022-07-19 11:01:53 +0900

    Add : 브랜드 탭 메뉴 클릭시 데이터 GET

--- week3/src/components/contents/Product.jsx
+++ week3/src/components/contents/Product.jsx
@@ -1,60 +1,40 @@
 import React, {useState} from 'react';
-import styles from './Product.css';
-// import api from '../../api/api';
-// import { productBrand } from '../../api/Product';
+import styles from '../../styles/Product.css';
 import axios from 'axios';
-import { useEffect } from 'react';
 
-const testdata = [
-  {
-    id: 0,
-    title: '브랜드1' 
-  },
-  {
-    id: 1,
-    title: '브랜드2' 
-  },
-  {
-    id: 2,
-    title: '브랜드3' 
-  }
-]
+const BRAND_DATA = [{ name: 'alva' }, { name: 'colourpop' }, { name: 'misa' }]
 
 const Product = () => {
-  async function test(){
+  const [currentTab, setCurrentTab] = useState(0);
+  const [brandList, setBrandList] = useState();
+
+  const getResultData = async function getBrandList(name){
     try {
-      const response = await axios.get("https://makeup-api.herokuapp.com/api/v1/products.json?brand=alva");
-      console.log(response)
+      const response = await axios.get(`${'https://makeup-api.herokuapp.com/api/v1/products.json?brand='}${name}`);
+      setBrandList(response.data);
     }
     catch (error) {
       console.log(error);
     }
   }
 
-  useEffect(()=>{
-    test();
-  })
-  
-
-  console.log(fetch);
-  const [currentTab, setCurrentTab] = useState(0);
-
-  const selectMenuHandler = (index) => {
+  const selectBrandHandler = (index, name) => {
     setCurrentTab(index);
+    getResultData(name);
   };
+
   return(
     <div className='productContainer'>
       <div className='tabMenu'>
-      {testdata.map((e, index)=>{
+      {BRAND_DATA.map((e, index)=>{
         return(
           <li key={index} className={`${index === currentTab ? 'tab focused' : 'tab'}`}
-            onClick={() => selectMenuHandler(index)}>
-          {e.title}
+            onClick={() => selectBrandHandler(index, e.name)}>
+          {e.name}
           </li>)})}
       </div>
       <div className='productWrapper'>
-        <div className='productItem'>
-          
+        <div className='productItem'>        
         </div>
       </div>
     </div>
