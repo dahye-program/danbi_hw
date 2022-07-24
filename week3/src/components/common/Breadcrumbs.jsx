e843a1eb3844a065c5dfead09d5ae52d7a01456b
dahye-program <dahye0729@sch.ac.kr>
2022-07-25 08:57:45 +0900

    Modify : Breadcrumbs

--- week3/src/components/common/Breadcrumbs.jsx
+++ week3/src/components/common/Breadcrumbs.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import styles from '../../styles/Breadcrumbs.css';
+import PropTypes from 'prop-types';
+import BREADCRUMB from '../../data/BREADCRUMB';
 
-const Breadcrumbs = ({menuName}) => {
+const Breadcrumbs = ({Name}) => {
   return(
-    <>
-      <div className='Breadcrumbs'>
-      <h3>{`Home > ${menuName} `}</h3>
-      </div>
-    </>
-)
-}
+    <div className='Breadcrumbs'>
+      {`Home > ${BREADCRUMB[Name]}`}
+    </div>
+)}
+
+// Breadcrumbs.propTypes = {
+//   Name: PropTypes.string
+// };
+
+// Breadcrumbs.defaultProps = {
+//   Name: ''
+// };
 
 export default Breadcrumbs;
---
+++ week3/src/data/BREADCRUMB.js
@@ -0,0 +1,14 @@
+const BREADCRUMB = {
+    dashboard: 'Dashboard',
+    product: 'Product',
+    customers: 'Customers',
+    message: 'Message',
+    message1: 'Message1',
+    help: 'Help',
+    help1: 'Help1',
+    settings: 'Settings',
+    password: 'Password',
+    signout: 'Signout'
+}
+
+export default BREADCRUMB;
